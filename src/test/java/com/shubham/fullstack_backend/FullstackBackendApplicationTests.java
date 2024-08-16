3a586ec6b2a535f79c20818fabf834dc2119f323
Shubham <sk192196@gmail.com>
2024-08-16 13:42:21 +0530

    Done

---
+++ src/test/java/com/shubham/fullstack_backend/FullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.fullstack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
